7b0f9fdcbe0f59773edd028642a8ab78c92d4f20
Md-net-440 <90133844+Md-net-440@users.noreply.github.com>
2025-03-07 08:24:55 +0530

    Final Todo

--- src/components/TodoBody.jsx
+++ src/components/TodoBody.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 const TodoBody = () => {
 
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
+    const [showFinished, setShowFinished] = useState(true);
 
     useEffect(() => {
         let todoString = localStorage.getItem("todos");
@@ -24,6 +27,11 @@ const TodoBody = () => {
     const saveToLocalStorage = (params) => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }
+
+    // useEffect(() => {
+    //     localStorage.setItem("todos", JSON.stringify(todos));
+    // }, [todos]); // This runs every time `todos` is updated
+    
 
     const handelChange = (e) => {
         setTodo(e.target.value);
@@ -67,35 +75,39 @@ const TodoBody = () => {
         saveToLocalStorage();
     }
 
+    const toggleFinished = () => {
+        setShowFinished(!showFinished)
+    }
+
   return (
     <div className="w-screen flex justify-center items-center my-5">
-      <div className="todo-container bg-blue-200 w-1/3 rounded-2xl p-5 flex justify-center items-center">
+      <div className="todo-container bg-blue-200 border-2 border-blue-950 w-1/3 rounded-2xl p-5 flex justify-center items-center">
         <div className="todo-content w-full">
             <div className="todo-top">
                 <div className="todo-add-section flex justify-between">
                     <input type="text" placeholder="Add Todo" name="todo" onChange={handelChange} value={todo} className="border-1 bg-white rounded-lg w-4/5 pl-3 h-10 mt-1"/>
-                    <button className="bg-blue-800 text-white px-5 rounded-lg hover:bg-blue-950 hover:scale-105 transition-all" onClick={handelAdd}>
+                    <button className="bg-blue-800 text-white px-5 rounded-lg hover:bg-blue-950 hover:scale-105 transition-all disabled:bg-blue-400 disabled:hover:scale-100" disabled={todo.length<=3} onClick={handelAdd}>
                         Add
                     </button>
                 </div>
                 <div className="todo-all-complete mt-2">
-                    <input type="checkbox" name="" id="" /> <span>All Completed Tasks</span>
+                    <input type="checkbox" onChange={toggleFinished} checked={showFinished} /> <span>All Completed Tasks</span>
                 </div>
             </div>
-            <div className="todo-body bg-blue-500 border-2 border-blue-950 rounded-lg mt-5 p-5 text-white">
+            <div className="todo-body bg-blue-300 border-2 border-blue-950 rounded-lg mt-5 p-5 text-blue-950 font-semibold">
                 {todos.length == 0 && <div className="text-center">No Todos Added!</div> }
                     {todos.map(items => {
-                        return (
+                        return (!items.isCompleted || showFinished) && (
                         <div key={items.id} className="todo-body-content flex gap-5 w-full justify-between my-2 border-b-1 py-1">
                             <div className="todo-check">
-                                <input type="checkbox" name={items.id} onChange={handelCheckBox} />
+                                <input type="checkbox" className="border-blue-950 bg-blue-950" name={items.id} onChange={handelCheckBox} checked={items.isCompleted}/>
                             </div>
                             <div className={`${items.isCompleted?"line-through":""} todo-task`}>
                                 {items.todo}
                             </div>
                             <div className="todo-operations flex gap-3">
-                                <button onClick={(e) => {handelEdit(e, items.id)}} className="bg-blue-200 text-black px-3 h-10 text-sm  rounded-lg hover:bg-blue-950 hover:scale-105 hover:text-white transition-all">Edit</button>
-                                <button onClick={(e) => {handelDelete(e, items.id)}} className="bg-blue-200 text-black px-3 h-10 text-sm  rounded-lg hover:bg-blue-950 hover:scale-105 hover:text-white transition-all">Delete</button>
+                                <button onClick={(e) => {handelEdit(e, items.id)}} className="bg-blue-200 text-blue-950 px-3 h-10 text-lg  rounded-lg hover:bg-blue-950 hover:scale-105 hover:text-white transition-all"><FaEdit /></button>
+                                <button onClick={(e) => {handelDelete(e, items.id)}} className="bg-blue-200 text-blue-950 px-3 h-10 text-2xl  rounded-lg hover:bg-blue-950 hover:scale-105 hover:text-white transition-all"><MdDeleteForever /></button>
                             </div>
                         </div>
                     )})}
